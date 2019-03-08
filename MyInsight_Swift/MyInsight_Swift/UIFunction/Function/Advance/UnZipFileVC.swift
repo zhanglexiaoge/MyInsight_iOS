3c3824d992ef0653e3042b3eeae278fb2c8488ea
MenglongSong <983174628@qq.com>
2019-03-08 14:44:16 +0800

    沙盒路径

--- MyInsight_Swift/MyInsight_Swift/UIFunction/Function/Advance/UnZipFileVC.swift
+++ MyInsight_Swift/MyInsight_Swift/UIFunction/Function/Advance/UnZipFileVC.swift
@@ -23,29 +23,33 @@ class UnZipFileVC: UIViewController {
     
     // MARK: 解压缩zip包
     func unArchiveZip() -> Void {
-        
+        // zip包的路径
         let myZipFilePath = Bundle.main.path(forResource: "weex_lan", ofType: "zip")
         print(myZipFilePath!)
-        //let documentDirectoryURL =  try! NSFileManager().URLForDirectory(.DocumentDirectory, inDomain: .UserDomainMask, appropriateForURL: nil, create: true)
         
+        //解压的目标路径
         let documentDirectoryURL =  try! FileManager().url(for: FileManager.SearchPathDirectory.documentDirectory, in: FileManager.SearchPathDomainMask.userDomainMask, appropriateFor: nil, create: true)
-        print("Documents dir:")
+        
+        let fileArray = try! FileManager.init().contentsOfDirectory(atPath: documentDirectoryURL.path)
+        if fileArray.contains("weex_lan") == false {
+            let status = SSZipArchive.unzipFile(atPath: myZipFilePath!, toDestination: documentDirectoryURL.path)
+            print("Zip file unzipped! \(status)")
+        }
+        let weex_lanPath = documentDirectoryURL.appendingPathComponent("weex_lan/dist")
+        debugPrint("解压后的路径内容: \(weex_lanPath)")
+        
+        
+        
+        let weex_lanArray = try! FileManager.init().contentsOfDirectory(atPath: weex_lanPath.path)
+        
+        debugPrint("解压后的路径内容AAAA : \(weex_lanArray)")
+        
         print(documentDirectoryURL.path)
-        //SSZipArchive.unzipFileAtPath(myZipFilePath, toDestination: documentDirectoryURL.path)
-        let status = SSZipArchive.unzipFile(atPath: myZipFilePath!, toDestination: documentDirectoryURL.path)
-        print("Zip file unzipped! \(status)")
+        // 解压zip包
+        
+        debugPrint("AAAA: \(try! FileManager.init().contentsOfDirectory(atPath: documentDirectoryURL.path))")
+        // 解压后文件夹路径
         
     }
-    
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
