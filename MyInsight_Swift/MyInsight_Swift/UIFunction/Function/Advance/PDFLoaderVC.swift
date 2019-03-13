8ced0ccb3a3a70476be3adb1254c259aa60edc3f
MenglongSong <983174628@qq.com>
2019-03-13 11:18:43 +0800

    删除web加载pdf的方式

--- MyInsight_Swift/MyInsight_Swift/UIFunction/Function/Advance/PDFLoaderVC.swift
+++ MyInsight_Swift/MyInsight_Swift/UIFunction/Function/Advance/PDFLoaderVC.swift
@@ -8,6 +8,7 @@
 //
 
 import UIKit
+import PDFKit
 
 class PDFLoaderVC: UIViewController {
 
@@ -16,42 +17,7 @@ class PDFLoaderVC: UIViewController {
         
         self.view.backgroundColor = UIColor.white
         
-        let pdfFilePath = Bundle.main.path(forResource: "compressed.tracemonkey-pldi-09", ofType: "pdf")
-        self.loadPDFFile(filePath: pdfFilePath!)
         
     }
     
-    // MARK: 加载PDF
-    func loadPDFFile(filePath: String) -> Void {
-        let webView = UIWebView(frame: self.view.bounds)
-        self.view.addSubview(webView)
-        
-
-        let viwerPath: String = Bundle.main.path(forResource: "viewer", ofType: "html")! // , inDirectory: "generic/web"
-        // %@?file=%@#page=1
-        var urlStr = viwerPath + "?file=\(filePath)#page=1"
-        urlStr = urlStr.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed)!
-        let request = URLRequest(url: URL(string: urlStr)!)
-        webView.loadRequest(request)
-    }
-    
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
-
-
-class PDFWebView: UIWebView {
-    
-    
-    
-}
-
