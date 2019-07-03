5482e156a6340e5525ec53527fb77ad9d052bf84
MenglongSong <983174628@qq.com>
2019-07-03 19:56:34 +0800

    gif

---
+++ MyInsight_Swift/MyInsight_Swift/UIFunction/Function/Basic/LoadGifVC.swift
@@ -0,0 +1,28 @@
+
+//
+//  LoadGifVC.swift
+//  MyInsight_Swift
+//
+//  Created by SongMenglong on 2019/7/3.
+//  Copyright © 2019 SongMengLong. All rights reserved.
+//
+
+import UIKit
+import SDWebImage
+import Kingfisher
+
+// 加载gif
+class LoadGifVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // https://img.alicdn.com/tfs/TB1Ep_9NVXXXXb8XVXXXXXXXXXX-74-74.gif
+        
+        
+        
+        
+    }
+    
+
+}
