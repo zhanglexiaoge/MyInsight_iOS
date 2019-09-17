6ee7576c72b58a719c65b2b04517a767016ee870
MenglongSong <983174628@qq.com>
2019-09-17 19:52:08 +0800

    grdb

---
+++ MyInsight_Swift/MyInsight_Swift/UIFunction/Function/Basic/GRDB/DataManager.swift
@@ -0,0 +1,38 @@
+//
+//  DataManager.swift
+//  MyInsight_Swift
+//
+//  Created by SongMenglong on 2019/9/17.
+//  Copyright © 2019 SongMengLong. All rights reserved.
+//
+
+import GRDB
+
+class DataManager: NSObject {
+
+    /// 初始化
+    var dbPool: DatabasePool?
+    var dbQueue: DatabaseQueue?
+    
+    
+//    func didChangesss() -> Void {
+//        do {
+//            dbPool = try DatabasePool(path: path)
+//            dbQueue = try DatabaseQueue(path: path)
+//        }catch {
+//
+//        }
+//    }
+}
+
+/*
+ http://swift-salaryman.com/grdb.php
+ 
+ Swift SQLite ORM 框架 - GRDB.swift 使用
+ https://blog.csdn.net/a794561799/article/details/94492714#_334
+ 
+ GRDB.swift as a Solution for iOS Database
+ https://www.netguru.com/codestories/grdb.swift-as-a-solution-for-ios-database
+ 
+ */
+
